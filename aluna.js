6cbdb931f75698964dffda56c2637784da14f903
Leo Battaglia <ms@raptorizm.com>
2022-01-12 17:14:18 +0100

    Version 0.0.3

--- aluna.js
+++ aluna.js
@@ -1,24 +1,15 @@
 /*Imports*/
+import * as config from "./aluna_config.js";
 import * as func from "./aluna_functions.js";
 
 /*Editor-Class*/
 class Editor{
-    bgEditor = "#1b1b1b";
-    bgLineActive = "#242424";
-    bgNumbers = "#181818";
-    bgScrollbar = "#151515";
-    bgScrollbarObject = "#303030";
-    bgScrollbarOverflow = "#101010";
-    bgVerticalLine = "#2b2b2b";
     caret;
     caretPosition;
     charHeight;
     charWidth;
     cursorPosition;
     editor;
-    fgEditor = "#eeeeee";
-    fgNumbers = "#555555";
-    fgNumbersActive = "#888888";
     firstNumber;
     frameLine;
     frameMain;
@@ -27,14 +18,9 @@ class Editor{
     frameEditorV;
     frameNumbers;
     lineHeight;
-    lineHeightPlus = 2;
-    lineVerticalWidth = 2;
     numbers;
-    numbersLength = 4;
-    padding = 5;
     scrollBarH;
     scrollBarV;
-    scrollBarWidth = 12;
     selection;
 
     constructor(){
@@ -42,7 +28,7 @@ class Editor{
     }
 
     addLine(content){
-        let div = func.initLine(this.lineHeight, this.padding);
+        let div = func.initLine(this.lineHeight);
         let subDiv = document.createElement("div");
         subDiv.setAttribute("class", "aluna_editor_line_content");
         subDiv.style.width = "fit-content";
@@ -66,20 +52,20 @@ class Editor{
     }
 
     init(){
-        this.frameMain = func.initFrameMain(this.fgEditor);
-        this.frameNumbers = func.initFrameNumbers(this.frameMain, this.bgNumbers, this.fgNumbers);
-        this.numbers = func.initNumbers(this.frameNumbers, this.padding);
+        this.frameMain = func.initFrameMain();
+        this.frameNumbers = func.initFrameNumbers(this.frameMain);
+        this.numbers = func.initNumbers(this.frameNumbers);
         this.firstNumber = document.getElementById("aluna_firstNumber");
-        this.frameLine = func.initVerticalLine(this.frameMain, this.bgVerticalLine, this.lineVerticalWidth);
-        this.frameEditorH = func.initFrameEditorHorizontal(this.frameMain, this.bgScrollbarOverflow);
+        this.frameLine = func.initVerticalLine(this.frameMain);
+        this.frameEditorH = func.initFrameEditorHorizontal(this.frameMain);
         this.frameEditorV = func.initFrameEditorVertical(this.frameEditorH);
-        this.scrollBarH = func.initScrollbarHorizontal(this.frameEditorH, this.bgScrollbar, this.bgScrollbarObject, this.scrollBarWidth);
-        this.frameEditor = func.initFrameEditor(this.frameEditorV, this.bgEditor);
-        this.scrollBarV = func.initScrollbarVertical(this.frameEditorV, this.bgScrollbar, this.bgScrollbarObject, this.scrollBarWidth);
-        this.editor = func.initEditor(this.frameEditor, this.padding);
+        this.scrollBarH = func.initScrollbarHorizontal(this.frameEditorH);
+        this.frameEditor = func.initFrameEditor(this.frameEditorV);
+        this.scrollBarV = func.initScrollbarVertical(this.frameEditorV);
+        this.editor = func.initEditor(this.frameEditor);
         this.initDefaultSizes();
         this.setDefaultSizes();
-        this.caret = func.initCaret(this.frameEditor, this.fgEditor, this.charHeight, this.padding);
+        this.caret = func.initCaret(this.frameEditor, this.charHeight);
         this.initListeners();
         this.addLine("ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ");
         this.addLine("01234567890123456789");
@@ -90,7 +76,7 @@ class Editor{
     initDefaultSizes(){
         this.charHeight = this.firstNumber.offsetHeight;
         this.charWidth = this.firstNumber.offsetWidth;
-        this.lineHeight = this.charHeight + this.lineHeightPlus;
+        this.lineHeight = this.charHeight + config.lineHeightPlus;
     }
 
     initListeners(){
@@ -160,10 +146,10 @@ class Editor{
             div.setAttribute("class", "aluna_number");
             div.innerHTML = (i + 1) + "";
             if(i === row){
-                div.style.color = this.fgNumbersActive;
+                div.style.color = config.fgNumbersActive;
                 div.innerHTML = "<b>" + div.innerHTML + "</b>";
             }else{
-                div.style.color = this.fgNumbers;
+                div.style.color = config.fgNumbers;
             }
             this.numbers.appendChild(div);
         }
@@ -180,14 +166,14 @@ class Editor{
 
         //TODO: Insert Scrollbars into Calculations
 
-        let top = coords.y + this.frameEditor.offsetTop - (coords.y % this.lineHeight) + this.padding + 5;
+        let top = coords.y + this.frameEditor.offsetTop - (coords.y % this.lineHeight) + config.padding + 5;
         if(top > this.editor.offsetHeight){
-            top = this.editor.offsetTop + this.padding + this.editor.offsetHeight - this.lineHeight;
+            top = this.editor.offsetTop + config.padding + this.editor.offsetHeight - this.lineHeight;
         }
         this.caret.style.top = top + "px";
-        let rowNr = Math.floor((top - this.editor.offsetTop - this.padding) / this.lineHeight);
+        let rowNr = Math.floor((top - this.editor.offsetTop - config.padding) / this.lineHeight);
         let row = this.editor.children[rowNr].children[0];
-        let left = func.getLeft(this.editor, row, coords, this.charWidth, this.padding);
+        let left = func.getLeft(this.editor, row, coords, this.charWidth, config.padding);
         this.caret.style.left = left + "px";
         let colNr = Math.floor((left - this.editor.offsetLeft) / this.charWidth);
         this.caretPosition = {
@@ -197,7 +183,7 @@ class Editor{
         if(func.isSelection(this.cursorPosition, coords, this.charWidth, this.lineHeight)){
             this.caret.focus();
         }else{
-            let left2 = func.getLeft(this.editor, row, this.cursorPosition, this.charWidth, this.padding);
+            let left2 = func.getLeft(this.editor, row, this.cursorPosition, this.charWidth, config.padding);
             let colNr2 = Math.floor((left2 - this.editor.offsetLeft) / this.charWidth);
             let startAndLength = func.getColumnStartAndLength(colNr, colNr2);
             this.selection = {
@@ -207,13 +193,13 @@ class Editor{
             };
         }
         this.cursorPosition = undefined;
-        func.showActiveRow(this.editor, rowNr, this.bgLineActive);
+        func.showActiveRow(this.editor, rowNr, config.bgLineActive);
         this.resetNumbers(rowNr);
     }
 
     setDefaultSizes(){
         this.frameNumbers.style.maxHeight = this.frameNumbers.offsetHeight + "px";
-        let width = (this.numbersLength * this.firstNumber.offsetWidth) + (2 * this.padding);
+        let width = (config.numbersLength * this.firstNumber.offsetWidth) + (2 * config.padding);
         this.frameNumbers.style.maxWidth = width + "px";
         this.frameNumbers.style.minWidth = width + "px";
         this.frameNumbers.style.width = width + "px";
@@ -227,7 +213,7 @@ class Editor{
         let width = 0;
         for(let i = 0; i < this.editor.childElementCount; i++){
             if(this.editor.children[i].children[0].offsetWidth > width){
-                width = this.editor.children[i].children[0].offsetWidth + this.padding;
+                width = this.editor.children[i].children[0].offsetWidth + config.padding;
             }
         }
         if(width < this.frameEditor.offsetWidth){
